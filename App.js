6cbbdd8e2eaa88bf8ac6efbbcdeb405841d92acf
JOAO VICTOR PEREIRA DE OLIVEIRA <202002084111@alunos.estacio.br>
2023-03-30 20:57:30 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      />
+      <Text style={styles.appTitle}>Bem vindo a disciplina de Mobile</Text>
+      <Text style={styles.appName}>João Victor</Text>
+      <Text style={styles.appSala}>Sala E102</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#92a8d1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  appTitle: {
+    fontSize: 30,
+    color: 'black',
+    textAlign: 'center'
+  },
+  appName: {
+    fontSize: 20,
+    color: 'black',
+    textAlign: 'center'
+  },
+  appSala: {
+    fontSize: 15,
+    color: 'blue',
+    textAlign: 'center'
+  },
+  tinyLogo: {
+    width: 60,
+    height: 60,
+  }
+
 });
